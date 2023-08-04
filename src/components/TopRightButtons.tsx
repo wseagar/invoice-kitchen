4c6a9ab6accb3412695ab765bb523ee90ffb627f
Will Seagar <w.seagar@gmail.com>
2023-08-04 15:07:09 +1200

    Add loading state and success indicator for sending the pdf

--- src/components/TopRightButtons.tsx
+++ src/components/TopRightButtons.tsx
@@ -20,6 +20,7 @@ import { Input } from './ui/input';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
 import { Turnstile } from '@marsidev/react-turnstile';
 import { set } from 'mobx';
+import { ReloadIcon } from '@radix-ui/react-icons';
 
 const NewButton = () => {
   const [open, setOpen] = React.useState(false);
@@ -194,6 +195,108 @@ const PresetButton = () => {
     </>
   );
 };
+const EmailDialog = ({
+  open,
+  setOpen,
+  title,
+  description,
+  additionalDescription,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  title: string;
+  description: string;
+  additionalDescription?: string;
+}) => {
+  const { sendInvoice } = useAppStateStore();
+  const [token, setToken] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState('');
+  const [sent, setSent] = React.useState(false);
+
+  const send = async () => {
+    setLoading(true);
+    const result = await sendInvoice(email, token);
+    if (result) {
+      setLoading(false);
+      setSent(true);
+    } else {
+      setLoading(false);
+      setError(
+        `Something went wrong and the chef could not send your email. The chef is looking into it.`,
+      );
+    }
+  };
+
+  const content = () => {
+    return (
+      <>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+          {additionalDescription && (
+            <DialogDescription>{additionalDescription}</DialogDescription>
+          )}
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="email"
+              value={email}
+              className="col-span-3"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <Turnstile
+            siteKey="0x4AAAAAAAIMHH3XH4HezMXA"
+            onSuccess={setToken}
+            options={{
+              theme: 'light',
+              appearance: 'interaction-only',
+            }}
+          />
+        </div>
+        <DialogFooter>
+          <Button
+            type="submit"
+            disabled={!email || loading}
+            onClick={() => send()}
+          >
+            {loading && (
+              <>
+                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                Sending...
+              </>
+            )}
+            {!loading && 'Send'}
+          </Button>
+        </DialogFooter>
+      </>
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        {sent && (
+          <DialogHeader>
+            <DialogTitle>Success!</DialogTitle>
+            <DialogDescription>
+              The chef just delivered a hot PDF invoice direct to your inbox.
+            </DialogDescription>
+          </DialogHeader>
+        )}
+        {!sent && content()}
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 const AfterPrintPdfDialog = ({
   open,
@@ -201,61 +304,14 @@ const AfterPrintPdfDialog = ({
 }: {
   open: boolean;
   setOpen: (open: boolean) => void;
-}) => {
-  const { sendInvoice } = useAppStateStore();
-  const [token, setToken] = React.useState('');
-  const [email, setEmail] = React.useState('');
-
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Thanks for using Invoice Kitchen!</DialogTitle>
-          <DialogDescription>
-            Enter your email below and we&apos;ll send you a PDF copy of your
-            invoice for your records.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Email
-            </Label>
-            <Input
-              id="email"
-              value={email}
-              className="col-span-3"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <Turnstile
-            siteKey="0x4AAAAAAAIMHH3XH4HezMXA"
-            onSuccess={setToken}
-            options={{
-              theme: 'light',
-              appearance: 'interaction-only',
-            }}
-          />
-        </div>
-
-        <DialogFooter>
-          <Button
-            type="submit"
-            disabled={!email || !token}
-            onClick={() => {
-              sendInvoice(email, token);
-              setOpen(false);
-            }}
-          >
-            Send
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
+}) => (
+  <EmailDialog
+    open={open}
+    setOpen={setOpen}
+    title="Thanks for using Invoice Kitchen!"
+    description="Enter your email below and we'll send you a PDF copy of your invoice for your records."
+  />
+);
 
 const EmailMePdfDialog = ({
   open,
@@ -263,63 +319,15 @@ const EmailMePdfDialog = ({
 }: {
   open: boolean;
   setOpen: (open: boolean) => void;
-}) => {
-  const { sendInvoice } = useAppStateStore();
-  const [token, setToken] = React.useState('');
-  const [email, setEmail] = React.useState('');
-
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>PDF</DialogTitle>
-          <DialogDescription>
-            The Invoice Chef needs a moment to finish cooking.
-          </DialogDescription>
-          <DialogDescription>
-            Enter your email below and we&apos;ll email you a copy of your
-            invoice momentarily.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Email
-            </Label>
-            <Input
-              id="email"
-              value={email}
-              className="col-span-3"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <Turnstile
-            siteKey="0x4AAAAAAAIMHH3XH4HezMXA"
-            onSuccess={setToken}
-            options={{
-              theme: 'light',
-              appearance: 'interaction-only',
-            }}
-          />
-        </div>
-        <DialogFooter>
-          <Button
-            type="submit"
-            disabled={!email}
-            onClick={() => {
-              sendInvoice(email, token);
-              setOpen(false);
-            }}
-          >
-            Send
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
+}) => (
+  <EmailDialog
+    open={open}
+    setOpen={setOpen}
+    title="PDF"
+    description="The Invoice Chef needs a moment to finish cooking."
+    additionalDescription="Enter your email below and we'll email you a copy of your invoice momentarily."
+  />
+);
 
 export const TopRightButtons: React.FC = () => {
   return (
